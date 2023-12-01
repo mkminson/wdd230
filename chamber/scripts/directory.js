b597597a71e62d9fbd0c5a09b32438ecd36ffb83
mkminson <mminson@byui.edu>
2023-12-01 00:03:41 -0700

    updated JS

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,61 +1,5 @@
-// const baseURL = "https://mkminson.github.io/wdd230/";
-// const linksURL = "https://mkminson.github.io/wdd230/data/members.json";
-
-// const gridbutton = document.querySelector("#grid");
-// const listbutton = document.querySelector("#list");
-// const display = document.querySelector("article");
-
-// //example using arrow function
-// gridbutton.addEventListener("click", () => {
-//     display.classList.add("grid");
-//     display.classList.remove("list");
-// });
-
-// // example using defined function
-// listbutton.addEventListener("click", showList);
-
-// function showList() {
-//     display.classList.add("list");
-//     display.classList.remove("grid");
-// }
-
-// async function getBusinessData() {
-//     const response = await fetch(linksURL);
-//     const data = await response.json();
-//     displayBusinesses(data.directory);
-// }
-
-// getBusinessData();
-
-// const displayBusinesses = (businesses) => {
-//     businesses.forEach((business) => {
-//         let busList = document.createElement('ul');
-
-//         business.info.forEach((item) => {
-//             let listInfo = document.createElement('li');
-
-//             if (item.name || item.address || item["phone number"] || item.url || item.logo) {
-//                 listItem.textContent = `${item.name}: ${item.address}, Phone: ${item["phone number"]}, URL: ${item.url}}`;
-
-//                 if (item.logo) {
-//                     let logoImg = document.createElement('img');
-//                     logoImg.src = `${baseURL}${item.logo}`;
-//                     logoImg.alt = `${item.name} Logo`;
-//                     listItem.appendChild(logoImg);
-//                 }
-
-//                 busList.appendChild(listItem);
-//             }
-//         });
-
-//         display.appendChild(busList);
-
-//     });
-// }
-
-
-const baseURL = "https://mkminson.github.io/wdd230/";
-const linksURL = "https://mkminson.github.io/wdd230/data/members.json";
+const baseURL = "https://mkminson.github.io/wdd230/chamber/";
+const linksURL = "https://mkminson.github.io/wdd230/chamber/data/members.json";
 
 const gridButton = document.querySelector("#grid");
 const listButton = document.querySelector("#list");
@@ -88,9 +32,24 @@ const displayBusinesses = (businesses) => {
         business.info.forEach((item) => {
             let listItem = document.createElement('li');
 
-            if (item.name || item.address || item["phone number"] || item.url || item.logo) {
+            // Access the property names correctly
+            if (item.name || item.address || item["phone number"] || item.url || item.logo || item.membership) {
                 // Check if the property is present before creating a list item
-                listItem.textContent = `${item.name}: ${item.address}, Phone: ${item["phone number"]}, URL: ${item.url}`;
+                if (item.name) {
+                    listItem.textContent += `${item.name}`;
+                }
+                if (item.address) {
+                    listItem.textContent += ` | Address: ${item.address}`;
+                }
+                if (item["phone number"]) {
+                    listItem.textContent += ` | Phone: ${item["phone number"]}`;
+                }
+                if (item.url) {
+                    listItem.textContent += ` | URL: ${item.url}`;
+                }
+                if (item.membership) {
+                    listItem.textContent += ` | Membership: ${item.membership}`;
+                }
 
                 if (item.logo) {
                     let logoImg = document.createElement('img');
