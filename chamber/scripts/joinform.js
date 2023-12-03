6db895c96f41da0b280c7eb590e976ec633a33ba
mkminson <mminson@byui.edu>
2023-12-02 19:39:51 -0700

    Add title pattern check

--- chamber/scripts/joinform.js
+++ chamber/scripts/joinform.js
@@ -3,3 +3,23 @@ document.addEventListener("DOMContentLoaded", function () {
     var formattedDate = currentDate.toISOString();
     document.getElementById("timestamp").value = formattedDate;
 });
+
+const title = document.querySelector("#title");
+const message = document.querySelector("#formmessage");
+const titlePattern = /^[a-zA-Z\s-]{7,}$/;
+
+title.addEventListener("focusout", checkPattern);
+
+function checkPattern() {
+    if (!titlePattern.test(title.value)) {
+        message.textContent = "Please enter a title of at least 7 characters using only letters, spaces, or -.";
+        message.style.visibility = "show";
+        title.style.backgroundColor = "#fff0f3";
+        title.value = "";
+        email.focus();
+    } else {
+        message.style.display = "none";
+        email.style.backgroundColor = "#fff";
+        email.style.color = "#000";
+    }
+}
